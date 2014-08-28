063ebf825e7931d8687d3c982792ab47724928ba
kylefdoherty <kyledoherty22@gmail.com>
2014-08-28 13:52:07 -0400

    refactored predict_email method

--- predict_email.rb
+++ predict_email.rb
@@ -3,6 +3,16 @@ require_relative 'find_pattern'
 
 class PredictEmail
 
+  def predict_email(name, company)
+    patterns = FindPattern.new.find_patterns(company)
+    if patterns.first 
+      emails = patterns.map {|pat| self.send(pat.to_sym, name, company)}
+      "#{company} uses #{patterns.size} email #{pluralize_pattern(patterns)}: #{patterns_to_string(patterns)}. We predict #{name}'s email to be: #{emails_to_string(emails)}."
+    else 
+      "Sorry, we can't predict the email for this person."
+    end 
+  end 
+
   def patterns_to_string(patterns)
     patterns.join(", ")
   end 
@@ -35,25 +45,16 @@ class PredictEmail
     "#{first[0]}.#{last[0]}@#{company}"
   end 
 
-  def predict_email(name, company)
-    patterns = FindPattern.new.find_patterns(company)
-    return "Sorry, we can't predict the email for this person." unless patterns.first
-    
-    patterns_string = patterns_to_string(patterns)
-    num_of_patterns = patterns.size
-    pluralize_pattern = patterns.size > 1 ? 'patterns' : 'pattern'
-
-    emails = patterns.map {|pat| self.send(pat.to_sym, name, company)}
-    emails_string = emails_to_string(emails)
-
-    "#{company} uses #{num_of_patterns} email #{pluralize_pattern}: #{patterns_string}. We predict #{name}'s email to be: #{emails_string}."
-
+  def pluralize_pattern(patterns)
+    patterns.size > 1 ? 'patterns' : 'pattern'
   end 
 
 end 
 
 
+ 
 
+  
 
 
 
