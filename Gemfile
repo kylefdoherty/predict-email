3ce27ccc0b4b0126464a639351a4808f2ce83c95
kylefdoherty <kyledoherty22@gmail.com>
2014-08-29 11:04:03 -0400

    updated gemfile

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in hola_kyle.gemspec
 gemspec
+require 'rake'
+require 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require 'rspec'
+
+require 'coveralls'
+Coveralls.wear!
+
 require_relative '../lib/predict_email/find_pattern'
 require_relative '../lib/predict_email/predict_email'
